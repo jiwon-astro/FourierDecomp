9f6737375b28fd98269d9b07b0b5de000c6f2911
Jiwon Jang <starrynight@snu.ac.kr>
2026-04-02 17:26:47 +0900

    update

--- source/FourierDecomp/quality.py
+++ source/FourierDecomp/quality.py
@@ -130,12 +130,12 @@ def _calc_quality_single(sid, args_ft, theta_ft, flt, n_grid=50, statistic='medi
             'fval_grid':fval_grid_ft,'residual_grid':bin_res_ft, 'phase_mask':phase_mask_ft}
 
 
-def lightcurve_quality(sid, df_FD, n_grid=50, mode=None, bands=None,
+def lightcurve_quality(sid, df_FD, n_grid=50, mode=None, selected_bands=None,
                        statistic='median', period_key='P', epoch_key='E'):
     if mode is None: mode = get_data_config().mode
     cfg = get_data_config(mode)
     filters = cfg.filters
-    if bands is None: bands = filters # all-band
+    if selected_bands is None: selected_bands = filters # all-band
     M_MAX = params.M_MAX
 
     # -------------------
@@ -165,14 +165,14 @@ def lightcurve_quality(sid, df_FD, n_grid=50, mode=None, bands=None,
     quality_list = []
     for i, bm in enumerate(bmask):
         flt = filters[i]
-        if not flt in bands: continue
+        if not flt in selected_bands: continue
         args_ft = (t[bm], mag[bm], emag[bm])
         theta_ft = np.array([m0s[i], amps[i], As, Qs, P, E], dtype=object)
         q = _calc_quality_single(sid, args_ft, theta_ft, flt, n_grid=50, statistic='median')
         if q is not None: quality_list.append(q)
     return quality_list
 
-def build_quality_table(ids, df_FD, mode=None, bands=None, n_grid=50,
+def build_quality_table(ids, df_FD, mode=None, selected_bands=None, n_grid=50,
                         statistic='median', output_fpath=None, overwrite=True):
     if mode is None: mode = get_data_config().mode
 
@@ -184,7 +184,7 @@ def build_quality_table(ids, df_FD, mode=None, bands=None, n_grid=50,
                 df_FD=df_FD,
                 n_grid=n_grid,
                 mode=mode,
-                bands=bands,
+                selected_bands=selected_bands,
                 statistic=statistic,
             ) # -> list
             if len(row_i) > 0: rows += row_i
@@ -193,6 +193,7 @@ def build_quality_table(ids, df_FD, mode=None, bands=None, n_grid=50,
 
     out = Table(rows=rows)
     if output_fpath is not None:
+        output_fpath.parent.mkdir(parents=True, exist_ok=True)
         out.write(output_fpath, overwrite=overwrite)
     return out
 
